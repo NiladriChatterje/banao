3b95db5ecffe97a97e76913a53d400a02d0b3aa1
NiladriChatterje <radongtxgpu@gmail.com>
2023-04-25 00:20:28 +0530

    second

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import './Home.css';
 import Rect_3 from '../../assets/rect03.png';
 import { Link } from 'react-router-dom';
+import { Button, ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap';
 
 const Heading = ['All Posts(', 'Article', 'Event', 'Education', 'Job'];
 const Home = () => {
 
     return (
-        <><h1></h1>
+        <>
             <img src={Rect_3} alt='Banner' />
             <section id={'headings'}>
                 <div>
                     <span>
-                        {Heading?.map((item, i) => <Link key={i} to={`/${item}`} style={{ marginRight: '15px', color: 'grey' }} >{item}</Link>)}
+                        {Heading?.map((item, i) => <Link key={i} to={`/${item}`} style={{ marginRight: '15px', color: 'grey', textDecoration: 'none' }} >{item}</Link>)}
                     </span>
-                    <div><button>Write a Post</button>
-                        <button>Join Group</button></div>
+                    <div>
+
+                        <Button variant='secondary' className='me-3'>Write a Post</Button>
+                        <ButtonGroup>
+                            <DropdownButton as={ButtonGroup} title={` Join Group`} id="bg-nested-dropdown">
+                                <Dropdown.Item eventKey="Group 1">Group 1</Dropdown.Item>
+                                <Dropdown.Item eventKey="Group 1">Group 2</Dropdown.Item>
+                            </DropdownButton>
+                        </ButtonGroup>
+                    </div>
                 </div>
                 <div id='podcast_container'>
 
--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './Navbar.css';
 import ATG from '../../assets/Union1.png';
 import { BsFillCaretDownFill } from 'react-icons/bs';
+import { BiSearchAlt2 } from 'react-icons/bi'
 
 function Navbar() {
     return (
         <nav>
             <img src={ATG} alt='LOGO' />
-            <input type='text' placeholder={'Search for your favorite groups in ATG'} />
+            <input type='text' placeholder={` Search for your favorite groups in ATG`} />
             <span style={{ display: 'flex', placeItems: 'center', fontWeight: '700' }}>Create account.&nbsp;<a href="#">It's Free!</a><BsFillCaretDownFill style={{ width: '12px' }} /></span>
         </nav>
     );
